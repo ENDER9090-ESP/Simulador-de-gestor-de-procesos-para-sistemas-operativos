79885346a2ed4384232297ed675527990eb6d7d5
Jose Antonio Francisco Mogollón <joseantoniofranciscomogollon@gmail.com>
2026-05-17 20:39:36 -0600

    chore: generate build artifacts and assembly metadata

--- SimuladorGestorProcesos/obj/Debug/net8.0/SimuladorGestorProcesos.AssemblyInfo.cs
+++ SimuladorGestorProcesos/obj/Debug/net8.0/SimuladorGestorProcesos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimuladorGestorProcesos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8e5e28d25a481f1d0008a86c16a859d6c189a61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45903c0becf20e0ac8bbe3bc2c3bcc586910dfb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimuladorGestorProcesos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimuladorGestorProcesos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
